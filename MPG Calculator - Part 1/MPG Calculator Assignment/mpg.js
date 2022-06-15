64962398f17b92ec1dc1ebf034de2ed59fee824c
CottonpuffProgrammer <105884584+CottonpuffProgrammer@users.noreply.github.com>
2022-06-15 11:26:17 -0700

    Add files via upload
    Partially complete MPG HTML/TS

---
+++ MPG Calculator - Part 1/MPG Calculator Assignment/mpg.js
@@ -0,0 +1,44 @@
+/**
+ * Checks if form data is valid
+ * @returns {boolean} Returns true is all data is valid on the form, or false is something is invalid
+ */
+function isValid() {
+    //function should validate form entries
+    //and return true or false corresponding to validity
+    var miles = getElement("miles").value;
+    var gallons = getElement("gallons").value;
+    if (isNumber(miles) == true) {
+        getElement("mileserror").innerHTML = "Must be a number!";
+        return false;
+    }
+    else if (isNumber(gallons) == true) {
+        getElement("gallonserror").innerHTML = "Must be a number!";
+        return false;
+    } // the above two if statements check for invalid input
+    else if (isNumber(miles) == false && isNumber(gallons) == false) {
+        getElement("mileserror").innerHTML = "";
+        getElement("gallonserror").innerHTML = "";
+        return true;
+    } // the above checks for valid input
+    return false; // display error messages if inputs are invalid
+}
+// Returns whether input is a number or not
+function isNumber(isNum) {
+    return isNaN(isNum);
+}
+/**
+ * This function should be called when the calculate button is clicked
+*/
+function main() {
+    //check if data is valid
+    //if data is valid
+    if (isValid() == true) {
+        
+    }
+    //calculate MPG
+}
+/** document.getElementById().innerHTML */
+function getElement(id) {
+    // returns an element as a HTMLInputElement
+    return document.getElementById(id);
+}
